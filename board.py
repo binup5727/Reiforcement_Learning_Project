35c80d06ed0fa831da6d6a7fa16946790d18328a
binup5727 <binup5727@gmail.com>
2022-03-15 19:16:36 -0400

    q learning

--- board.py
+++ board.py
@@ -1,19 +1,50 @@
 from re import L
 import pygame
-
+import numpy as np
+import matplotlib.pyplot as plt
 class board:
-    def __init__(self, rows, col, win, screen):
+    def __init__(self, rows, col, screen):
         self.rows = rows
         self.col = col
         self.screen = screen
+        
+        
+
+
+        self.initialize()
+
+        
+
+        #print(win[0])
+        
+
+        
+    def initializeS(self):
+        
+        self.state = [3, 0]
+
+    def initializeP(self):
+        for i in range(self.rows):
+            for j in range(self.col):
+                if self.board[i][j] == 'p':
+                    self.board[i][j] = 0
+                    
+        self.board[self.state[0]][self.state[1]] = 'p'
+
+        
+
+        
+
+        
+    def initialize(self):
         self.board = []
         self.boardRect = []
         self.Q = []
         self.actions = ['up', 'down', 'left', 'right']
-        self.win = win
-        print(win[0])
-        
-        
+        self.win = [3, self.col - 1]
+        # self.nxtState = []
+        self.initializeS()
+        self.epsilon = .01
 
         for i in range(self.rows):
             self.board.append([])
@@ -27,14 +58,62 @@ class board:
 
                 pygame.draw.rect(self.screen, (0, 0, 255), self.boardRect[i][j])
 
-                for k in self.actions:
+                for k in range(4):
                     self.Q[i][j].append(0)
+                    
         
-        self.board[win[0]][win[1]] = 1
-        
-        print(self.Q)
-        print(self.board)
+        self.board[self.win[0]][self.win[1]] = 1
 
+        self.board[self.state[0]][self.state[1]] = 'p'
+        
+        
+    #reward for state
+    def reward(self, state):
+        if state == self.win:
+            return 1
+        else:
+            return 0
+
+    #choose a action ep greedy
+    def choose(self):
+
+        choice = np.random.random()
+        
+        #print(self.actions[np.random.randint(len(self.actions))])
+        if choice < 1 - self.epsilon:
+            print((self.Q[self.state[0]][self.state[1]]))
+            if np.sum(self.Q[self.state[0]][self.state[1]]) == 0:
+                action = self.actions[np.random.randint(0, len(self.actions))]
+            else:
+                
+                action = self.actions[np.argmax(self.Q[self.state[0]][self.state[1]])]
+
+
+        else:
+            action = self.actions[np.random.randint(0, len(self.actions))]
+
+
+
+        return action
+
+    #take action to get next state.
+    # if on edge or goes into wall state doesn't change.
+    def nextState(self, action):
+        print(self.state)
+        if action == 'up' and (self.state[0] > 0 and self.board[self.state[0] - 1][self.state[1]] != 'p'):
+            return [self.state[0] - 1, self.state[1]]
+
+        elif action == 'down' and (self.state[0] < self.rows - 1 and self.board[self.state[0] + 1][self.state[1]] != 'p'):
+            return [self.state[0] + 1, self.state[1]]
+
+        elif action == 'right' and (self.state[1] < self.col - 1 and self.board[self.state[0]][self.state[1] + 1] != 'p'):
+            return [self.state[0], self.state[1] + 1]
+
+        elif action == 'left' and (self.state[1] > 0 and self.board[self.state[0]][self.state[1] - 1] != 'p'):
+            return [self.state[0], self.state[1] - 1]
+
+        else:
+            return [self.state[0], self.state[1]]
 
                 
     def draw(self):
@@ -55,16 +134,28 @@ class player:
 
     def __init__(self, rows, col, screen) -> None:
         self.screen = screen 
-        self.board = board(rows, col, [3, 5],self.screen)
+        self.board = board(rows, col,self.screen)
+
         self.player = pygame.image.load('index.png')
         self.player = pygame.transform.scale(self.player, (10, 10)).convert()
         self.playerRec = self.player.get_rect()
-        self.playerRec.x = self.board.boardRect[0][-1].x + 20
-        self.playerRec.y = self.board.boardRect[0][-1].y
         self.screen.blit(self.player, self.playerRec)
-    
-    def move(self):
+        self.episodes = 50
+        self.alpha = .5
+        self.gamma = .9
+
+        self.epcount_q = np.zeros(self.episodes)
+        for i in range(4):
+            self.epcount_q += self.play_q_learning()
         
+        plt.plot(self.epcount_q)
+
+        self.epcount_sarsa = self.play_sarsa()
+
+        
+    
+    def showPlayer(self):
+        self.board.draw()
         for i in range(self.board.rows):
             for j in range(self.board.col):
                 if self.board.board[i][j] == 'p':
@@ -72,5 +163,94 @@ class player:
                     self.playerRec.center = self.board.boardRect[i][j].center
                     #print(self.board.boardRect[i][j].center)
                     self.screen.blit(self.player, self.playerRec)
+        
+        pygame.display.update()
+
+    def play_q_learning(self):
+
+        self.board.initialize()
+        
+
+        epcount_q = np.zeros(self.episodes)
+
+        for i in range(self.episodes):
+            self.board.initializeS()
+            self.board.initializeP()
+            reward = 0
+            while reward == 0:
+
+                action = self.board.choose()
+                nxtState = self.board.nextState(action)
+                reward = self.board.reward(nxtState)
+                if reward == 1:
+                    print(reward)
+                print(self.board.state)
+
+                nxtQ = np.max(self.board.Q[nxtState[0]][nxtState[1]])
+                currentQ = self.board.Q[self.board.state[0]][self.board.state[1]]
+                #print(currentQ, nxtQ, self.board.board)
+
+                currentQ[self.board.actions.index(action)] = currentQ[self.board.actions.index(action)] + self.alpha * (reward + self.gamma * nxtQ - currentQ[self.board.actions.index(action)])
+                
+                self.board.board[self.board.state[0]][self.board.state[1]] = 0
+                self.board.state = nxtState
+                self.board.board[self.board.state[0]][self.board.state[1]] = 'p'
+                self.showPlayer()
+                epcount_q[i] += 1
+        #self.board.draw()
+
+        return epcount_q      
+        # print(self.epcount_q)
+        # plt.plot(self.epcount_q)
 
 
+    def play_sarsa(self):
+
+        self.board.initialize()
+        
+
+        epcount_q = np.zeros(self.episodes)
+
+        for i in range(self.episodes):
+            self.board.initializeS()
+            self.board.initializeP()
+            reward = 0
+            while reward == 0:
+
+                action = self.board.choose()
+                nxtState = self.board.nextState(action)
+                reward = self.board.reward(nxtState)
+                if reward == 1:
+                    print(reward)
+                print(self.board.state)
+
+                nxtQ = np.max(self.board.Q[nxtState[0]][nxtState[1]])
+                currentQ = self.board.Q[self.board.state[0]][self.board.state[1]]
+                #print(currentQ, nxtQ, self.board.board)
+
+                currentQ[self.board.actions.index(action)] = currentQ[self.board.actions.index(action)] + self.alpha * (reward + self.gamma * nxtQ - currentQ[self.board.actions.index(action)])
+                
+                self.board.board[self.board.state[0]][self.board.state[1]] = 0
+                self.board.state = nxtState
+                self.board.board[self.board.state[0]][self.board.state[1]] = 'p'
+                self.showPlayer()
+                epcount_q[i] += 1
+        #self.board.draw()
+
+        return epcount_q      
+        
+                
+
+
+
+
+
+
+
+
+
+
+
+# self.board[self.state[0]][self.state[1]] = 0
+# self.state = [self.state[0] - 1], [self.state[1]]
+# self.board[self.state[0]][self.state[1]] = 'p'
--- tester.py
+++ tester.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
 #--- Q learning
-q_board = player(5, 6, screen)
+q_board = player(6, 9, screen)
 
 while True:
     for event in pygame.event.get():
@@ -18,20 +18,22 @@ while True:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            if q_board.playerRec.collidepoint(mouse_pos):
-                print(q_board.board.board)
+            # if q_board.playerRec.collidepoint(mouse_pos):
+            #     print(q_board.board.board)
                 
 
-                row = int(input('row: '))
-                col = int(input('column: '))
-                q_board.board.board[row][col] = 'p'
+            #     row = int(input('row: '))
+            #     col = int(input('column: '))
+            #     q_board.board.board[row][col] = 'p'
 
                 
             for i in range(q_board.board.rows):
                 for j in range(q_board.board.col):
                     #print(q_board.board.boardRect[i][j])
                     if q_board.board.boardRect[i][j].collidepoint(mouse_pos) and not (q_board.board.board[i][j] == 1 or q_board.board.board[i][j] == 'p'):
+                        
                         q_board.board.board[i][j] = '*'
+                        
             
 
 
@@ -39,7 +41,7 @@ while True:
         #screen.fill((0,0,0))
 
         q_board.board.draw()
-        q_board.move()
+        q_board.showPlayer()
         
 
 
